108b76683067b6453d17b291dec2b6bb20437ed1
MartinR01 <martin.ryba01@gmail.com>
2018-01-05 04:47:30 +0100

    merged the win / loss screens into one - makes it easier to change stuff

---
+++ DeSocialize/app/src/main/java/com/a000webhostapp/desocialize/desocialize/Result.java
@@ -0,0 +1,9 @@
+package com.a000webhostapp.desocialize.desocialize;
+
+/**
+ * Created by next4 on 05.01.2018.
+ */
+
+public enum Result {
+    WIN, LOSS;
+}
--- DeSocialize/app/src/main/java/com/a000webhostapp/desocialize/desocialize/SingleplayerActivity.java
+++ DeSocialize/app/src/main/java/com/a000webhostapp/desocialize/desocialize/SingleplayerActivity.java
@@ -6,7 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.os.PowerManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -18,16 +20,21 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class SingleplayerActivity extends AppCompatActivity {
+    Result lastResult = Result.LOSS;
+    View resultOverlay;
+    TextView resultTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleplayer);
+        Resources resources = getResources();
 
         // setup phone unlock receiver
         PhoneUnlockedReceiver.register(this, new SPUnlock());
@@ -35,52 +42,56 @@ public class SingleplayerActivity extends AppCompatActivity {
 
         // setup numberPicker
         NumberPicker numberPicker = findViewById(R.id.minutesPicker);
-        numberPicker.setMinValue(R.integer.min_duration);
-        numberPicker.setMaxValue(R.integer.max_duration);
-        numberPicker.setValue(R.integer.def_duration);
+        numberPicker.setMinValue(resources.getInteger(R.integer.min_duration));
+        numberPicker.setMaxValue(resources.getInteger(R.integer.max_duration));
+        numberPicker.setValue(resources.getInteger(R.integer.def_duration));
 
 
         // setup win loss screen
-        ImageButton imageButton = findViewById(R.id.replayLossBT);
+        resultOverlay = findViewById(R.id.resultOverlay);
+        resultTV = findViewById(R.id.resultTV);
+        ImageButton imageButton = findViewById(R.id.replayBT);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 playAgain();
             }
         });
-        imageButton = findViewById(R.id.replayWinBT);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                playAgain();
-            }
-        });
-
-
     }
 
     /**
      * Displays win screen when user wins
      */
     private void win(){
-        Toast.makeText(this, "good", Toast.LENGTH_SHORT).show();
-        findViewById(R.id.winOverlay).setVisibility(View.VISIBLE);
+        //Toast.makeText(this, "good", Toast.LENGTH_SHORT).show();
+        if(lastResult.equals(Result.LOSS)){
+            resultTV.setText(R.string.youWin);
+            resultOverlay.setBackgroundColor(getResources().getColor(R.color.colorWin));
+        }
+
+        lastResult = Result.WIN;
+        findViewById(R.id.resultOverlay).setVisibility(View.VISIBLE);
     }
 
     /**
      * Displays lose screen when user loses
      */
     private void loss(){
-        Toast.makeText(this, "unlocked too early", Toast.LENGTH_SHORT).show();
-        findViewById(R.id.lossOverlay).setVisibility(View.VISIBLE);
+        //Toast.makeText(this, "unlocked too early", Toast.LENGTH_SHORT).show();
+        if(lastResult.equals(Result.WIN)){
+            resultTV.setText(R.string.youLost);
+            resultOverlay.setBackgroundColor(getResources().getColor(R.color.colorLoss));
+        }
+
+        lastResult = Result.LOSS;
+        findViewById(R.id.resultOverlay).setVisibility(View.VISIBLE);
     }
 
     /**
      * Makes win / lose screen invisible again when user clicks on "play again" button
      */
     private void playAgain(){
-        findViewById(R.id.winOverlay).setVisibility(View.GONE);
-        findViewById(R.id.lossOverlay).setVisibility(View.GONE);
+        findViewById(R.id.resultOverlay).setVisibility(View.GONE);
     }
 
     /**
