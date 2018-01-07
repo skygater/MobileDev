d7ab5f8259ec2f1d0dd602a77f9d8935dfa34cc8
Unknown <dariodaka@yahoo.com>
2018-01-07 14:41:48 +0100

    Main Menu complete

--- DeSocialize/app/src/main/java/com/a000webhostapp/desocialize/desocialize/MenuActivity.java
+++ DeSocialize/app/src/main/java/com/a000webhostapp/desocialize/desocialize/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.a000webhostapp.desocialize.desocialize;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -49,4 +50,11 @@ public void multiplayer (View view ){
     finish();
 
 }
+
+public void profile (View view) {
+    Intent next = new Intent( MenuActivity.this, ContactsContract.Profile.class);
+    startActivity(next);
+    finish();
+}
+
 }
